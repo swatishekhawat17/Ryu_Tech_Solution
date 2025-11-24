4e59b62b4dd0ac8d09d0f15595c913b94015bb2a
Swati-SJML <swati.shekhawat@syrmajohari.com>
2025-11-24 16:47:28 +0530

    -- added content to service page--

---
+++ app/services/page.tsx
@@ -0,0 +1,13 @@
+// app/Service/page.tsx
+import React from "react";
+
+const ServicePage = () => {
+  return (
+    <div>
+      <h1>Service</h1>
+      <p>Welcome to our company!</p>
+    </div>
+  );
+};
+
+export default ServicePage; // ✅ Default export is required
